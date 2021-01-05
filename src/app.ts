3eb57293864397ddba2b134cadbe06a978a9efb0
Theo <ferreiratheosa@gmail.com>
2021-01-05 06:27:25 +0200

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,1 @@
-const button = document.querySelector("button")!;
-
-function clickHandler(msg: string) {
-  console.log('Clicked! ' + msg);
-}
-//a comment will not be compiled
-if (button){
-  button.addEventListener("click", clickHandler.bind(null,'Your Here'));
-}
+// Code goes here!
