8dbadeef37a78a2a66bf55434e2f2a11170847e0
Theo <ferreiratheosa@gmail.com>
2021-01-06 08:38:36 +0200

    generics completed

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,2 @@
 "use strict";
-const names = [];
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("This is done!");
-    }, 2000);
-});
-promise.then(data => {
-    data.split(' ');
-});
 //# sourceMappingURL=app.js.map
